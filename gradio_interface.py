3d5f1531527ce8dd5f607b97ebae9b84ecd091eb
samsniderheld <samsniderheld@gmail.com>
2023-09-13 15:21:32 -0400

    Update gradio_interface.py

--- gradio_interface.py
+++ gradio_interface.py
@@ -5,14 +5,13 @@ import time
 
 import cv2
 import gradio as gr
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageFilter
 import torch
 
 from diffusers.utils import load_image
 
-from pipelines.pipelines import get_control_net_pipe, get_inpaint_pipe
+from pipelines.pipelines import get_control_net_pipe, get_img2img_pipe
 from utils import overlay_images, blend_image
 
 def parse_args():
@@ -77,8 +76,11 @@ def parse_args():
 
 args = parse_args()
 
+os.makedirs("temp_textures", exist_ok=True)
 control_net_pipe, control_proc = get_control_net_pipe(args.controlnet_path,args.base_texture_model)
 control_net_img = load_image(args.input_texture)
+
+img2img_pipe, img2img_proc = get_img2img_pipe(args.base_img2img_model)
 
 def generate_texture(ingredient,controlnet_img,controlnet_conditioning_scale,steps,cfg):
     #generates the texture
@@ -113,11 +115,51 @@ def generate_texture(ingredient,controlnet_img,controlnet_conditioning_scale,ste
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"The script took {elapsed_time:.2f} seconds to execute.")
+
+    out_img = cv2.cvtColor(np.uint8(texture_image),cv2.COLOR_BGR2RGB)
+    cv2.imwrite(f"temp_textures/texture.jpg", out_img)
     
     return texture_image
 
-# def generate_burger():
-#     #generates the burger
+def generate_burger(ingredient,strength,steps,cfg):
+
+    start_time = time.time()
+
+    burger_prompt = f"""image a burger with {ingredient}+++, photorealistic photography, 
+    8k uhd, full framed, photorealistic photography, dslr, soft lighting, 
+    high quality, Fujifilm XT3\n\n"""
+
+    img2img_embeds = img2img_proc(texture_prompt)
+
+    negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
+
+    negative_img2img_embeds = img2img_proc(negative_prompt)
+
+    start_time = time.time()
+    
+    random_seed = random.randrange(0,100000)
+
+    texture = Image.open("temp_textures/texture.jpg").convert("RGBA")
+    template = Image.open(args.template).convert("RGBA")
+    input_img = overlay_images(texture,template)
+     
+    image = img2img_pipe(prompt_embeds=img2img_embeds,
+                    negative_prompt_embeds = negative_img2img_embeds,
+                    image= input_img,
+                    strength = img2img_strength,
+                    num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
+                    guidance_scale = cfg).images[0]
+    
+    image = blend_image(image,input_img,mask,args.mask_blur)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"The script took {elapsed_time:.2f} seconds to execute.")
+
+    return image
+    
+
+    #generates the burger
 
 # def save_config():
 #     #saves the config
@@ -130,7 +172,7 @@ with gr.Blocks() as demo:
             with gr.Column():
                 controlnet_prompt_input = gr.Textbox(label="prompt")
                 controlnet_input_img = gr.Image(label="input img")
-                controlnet_controlnet_conditioning_scale_input = gr.Slider(0, 1, 
+                controlnet_conditioning_scale_input = gr.Slider(0, 1, 
                     value=args.controlnet_str, label="controlnet_conditioning_scale")
                 controlnet_steps_input = gr.Slider(0, 150, value=args.steps,
                     label="number of diffusion steps")
@@ -139,7 +181,7 @@ with gr.Blocks() as demo:
                 controlnet_inputs = [
                     controlnet_prompt_input,
                     controlnet_input_img,
-                    controlnet_controlnet_conditioning_scale_input,
+                    controlnet_conditioning_scale_input,
                     controlnet_steps_input,
                     controlnet_cfg_input,
                 ]
@@ -152,8 +194,37 @@ with gr.Blocks() as demo:
 
             controlnet_submit = gr.Button("Submit")
 
+    #burger gen row
+    with gr.Row():
+
+            with gr.Column():
+                img2img_strength = gr.Slider(0, 1, 
+                    value=args.img2img_strength, 
+                    label="img2img strength")
+                img2img_steps_input = gr.Slider(0, 150, value=args.steps,
+                    label="number of diffusion steps")
+                img2img_cfg_input = gr.Slider(0,30,value=args.cfg_scale,
+                                                 label="cfg scale")
+
+                img2img_inputs = [
+                    #use the same prompt from step 1
+                    controlnet_prompt_input,
+                    img2img_strength,
+                    img2img_steps_input,
+                    img2img_cfg_input,
+                ]
+
+            with gr.Column():
+
+                img2img_output = gr.Image()
+
+    with gr.Row():
+
+            img2img_submit = gr.Button("Submit")
+
 
     controlnet_submit.click(generate_texture,inputs=controlnet_inputs,outputs=controlnet_output)
+    img2img_submit.click(generate_burger,inputs=img2img_inputs,outputs=img2img_output)
 
 
 if __name__ == "__main__":
