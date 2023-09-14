626a96020d9ca4ed1a39d45ca8d2acf8e15fa8b3
samsniderheld <samsniderheld@gmail.com>
2023-09-14 16:45:35 -0400

    giving generate_textures the ability to run through a csv

--- generate_textures.py
+++ generate_textures.py
@@ -9,13 +9,10 @@ import numpy as np
 import cv2
 import torch
 
-
 from diffusers.utils import load_image
 
-
 from pipelines.pipelines import get_control_net_pipe
-
-
+from utils import read_ingredients_from_csv
 
 def parse_args():
     """
@@ -43,6 +40,9 @@ def parse_args():
         help='The controlnet model we are using.')
     parser.add_argument(
         '--ingredient', type=str, default='avocado', 
+        help='The ingredient texture we want to generate.')
+    parser.add_argument(
+        '--csv_file', type=str, default=None, 
         help='The ingredient texture we want to generate.')
     parser.add_argument(
         '--dims', type=int, default=512, 
@@ -77,38 +77,79 @@ control_net_pipe, compel_proc = get_control_net_pipe(args.controlnet_path,args.b
 control_net_img = load_image(args.input_texture)
 
 
+if(not args.csv_file):
 
-prompt = f"""food-texture, a 2D texture of {args.ingredient}+++, layered++, side view, 
-full framed, photorealistic photography, 8k uhd, dslr, soft lighting, high quality, 
-Fujifilm XT3\n\n"""
+    prompt = f"""a 2D texture of {args.ingredient}+++, layered++, side view, 
+    full framed, photorealistic photography, 8k uhd, dslr, soft lighting, high quality, 
+    Fujifilm XT3\n\n"""
 
-prompt_embeds = compel_proc(prompt)
+    prompt_embeds = compel_proc(prompt)
 
-negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
+    negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
 
-negative_prompt_embeds = compel_proc(negative_prompt)
+    negative_prompt_embeds = compel_proc(negative_prompt)
 
-for i in range(args.num_samples):
+    for i in range(args.num_samples):
 
-    start_time = time.time()
-    
-    random_seed = random.randrange(0,100000)
+        start_time = time.time()
+        
+        random_seed = random.randrange(0,100000)
 
-    image = control_net_pipe(prompt_embeds=prompt_embeds,
-                    negative_prompt_embeds = negative_prompt_embeds,
-                    image= control_net_img,
-                    controlnet_conditioning_scale=controlnet_conditioning_scale,
-                    height=height,
-                    width=width,
-                    num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
-                    guidance_scale = cfg).images[0]
+        image = control_net_pipe(prompt_embeds=prompt_embeds,
+                        negative_prompt_embeds = negative_prompt_embeds,
+                        image= control_net_img,
+                        controlnet_conditioning_scale=controlnet_conditioning_scale,
+                        height=height,
+                        width=width,
+                        num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
+                        guidance_scale = cfg).images[0]
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"The script took {elapsed_time:.2f} seconds to execute.")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"The script took {elapsed_time:.2f} seconds to execute.")
 
-    out_img = cv2.cvtColor(np.uint8(image),cv2.COLOR_BGR2RGB)
-    cv2.imwrite(f"{args.output_dir}/{i:04d}.jpg", out_img)
+        out_img = cv2.cvtColor(np.uint8(image),cv2.COLOR_BGR2RGB)
+        cv2.imwrite(f"{args.output_dir}/{args.ingredient}/{i:04d}.jpg", out_img)
+
+else:
+
+    ingredients = read_ingredients_from_csv(args.csv_file)
+
+    for ingredient in ingredients:
+
+        prompt = f"""a 2D texture of {ingredient}+++, layered++, side view, 
+        full framed, photorealistic photography, 8k uhd, dslr, soft lighting, high quality, 
+        Fujifilm XT3\n\n"""
+
+        prompt_embeds = compel_proc(prompt)
+
+        negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
+
+        negative_prompt_embeds = compel_proc(negative_prompt)
+
+        for i in range(args.num_samples):
+
+            start_time = time.time()
+            
+            random_seed = random.randrange(0,100000)
+
+            image = control_net_pipe(prompt_embeds=prompt_embeds,
+                            negative_prompt_embeds = negative_prompt_embeds,
+                            image= control_net_img,
+                            controlnet_conditioning_scale=controlnet_conditioning_scale,
+                            height=height,
+                            width=width,
+                            num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
+                            guidance_scale = cfg).images[0]
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"The script took {elapsed_time:.2f} seconds to execute.")
+
+            out_img = cv2.cvtColor(np.uint8(image),cv2.COLOR_BGR2RGB)
+            cv2.imwrite(f"{args.output_dir}/{ingredient}/{i:04d}.jpg", out_img)
+
+
 
 if torch.cuda.is_available():
     torch.cuda.empty_cache()
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
+import csv
 from PIL import Image, ImageFilter
-
 
 def overlay_images(background, overlay):
     # Resize overlay image to fit the background
@@ -19,3 +19,14 @@ def blend_image(inpainted, original, mask, blur=3):
     mask = mask.filter(ImageFilter.GaussianBlur(blur))
     # Blend images together
     return Image.composite(inpainted.convert('RGBA'), original.convert('RGBA'), mask).convert('RGBA')
+
+
+def read_ingredients_from_csv(file_path):
+    """Read ingredients from a CSV file and return them as a list."""
+    ingredients = []
+    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header if there is any
+        for row in reader:
+            ingredients.append(row[0])
+    return ingredients
