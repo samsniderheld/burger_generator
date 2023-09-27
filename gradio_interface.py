85431405866515276e83b7ecca05fcbd90fe7dca
samsniderheld <samsniderheld@gmail.com>
2023-09-27 08:40:19 -0400

    reconfigure gradio inteface

--- gradio_interface.py
+++ gradio_interface.py
@@ -26,7 +26,7 @@ Example:
 """
 
 import sys
-sys.path.append('../')
+# sys.path.append('../')
 import argparse
 import os
 import random
@@ -41,7 +41,8 @@ import torch
 from diffusers.utils import load_image
 
 from pipelines.pipelines import get_control_net_pipe, get_img2img_pipe
-from utils import overlay_images, blend_image
+from utils import (overlay_images, blend_image, 
+                   generate_template_and_mask, composite_ingredients)
 
 def parse_args():
     """
@@ -61,6 +62,9 @@ def parse_args():
     parser.add_argument(
         '--template', type=str, default='burger_templates/burger_template.png', 
         help='The burger template')
+    parser.add_argument(
+        '--overlay_dir', type=str, default='burger_templates/', 
+        help='The burger overlay')
     parser.add_argument(
         '--mask', type=str, default='burger_templates/burger_mask.png', 
         help='The burger mask')
@@ -111,44 +115,65 @@ control_net_img = load_image(args.input_texture)
 
 img2img_pipe, img2img_proc = get_img2img_pipe(args.base_img2img_model)
 
-def generate_texture(ingredient,controlnet_img,controlnet_conditioning_scale,steps,cfg):
+all_textures = []
+template = None
+composite = None
+mask = None
+
+def generate_texture(ingredients,controlnet_img,controlnet_conditioning_scale,steps,cfg):
     #generates the texture
+    global all_textures
+    all_textures = []
 
     start_time = time.time()
 
-    texture_prompt = f"""food-texture, a 2D texture of {ingredient}+++, layered++, side view, 
-    full framed, photorealistic photography, 8k uhd, dslr, soft lighting, high quality, 
-    Fujifilm XT3\n\n"""
+    ingredients = ingredients.split(",")
 
-    control_embeds = control_proc(texture_prompt)
+    for ingredient in ingredients:
 
-    negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
+        texture_prompt = f"""food-texture, a 2D texture of {ingredient}+++, layered++, side view, 
+        full framed, photorealistic photography, 8k uhd, dslr, soft lighting, high quality, 
+        Fujifilm XT3\n\n"""
 
-    negative_control_embeds = control_proc(negative_prompt)
+        control_embeds = control_proc(texture_prompt)
 
-    start_time = time.time()
-    
-    random_seed = random.randrange(0,100000)
+        negative_prompt = f'illustration, sketch, drawing, poor quality, low quality'
 
-    controlnet_img = Image.fromarray(controlnet_img)
+        negative_control_embeds = control_proc(negative_prompt)
 
-    texture_img = control_net_pipe(prompt_embeds=control_embeds,
-                    negative_prompt_embeds = negative_control_embeds,
-                    image= controlnet_img,
-                    controlnet_conditioning_scale=controlnet_conditioning_scale,
-                    height=512,
-                    width=512,
-                    num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
-                    guidance_scale = cfg).images[0]
-    
+        start_time = time.time()
+        
+        random_seed = random.randrange(0,100000)
+
+        controlnet_img = Image.fromarray(controlnet_img)
+
+        texture_img = control_net_pipe(prompt_embeds=control_embeds,
+                        negative_prompt_embeds = negative_control_embeds,
+                        image= controlnet_img,
+                        controlnet_conditioning_scale=controlnet_conditioning_scale,
+                        height=512,
+                        width=512,
+                        num_inference_steps=steps, generator=torch.Generator(device='cuda').manual_seed(random_seed),
+                        guidance_scale = cfg).images[0]
+        
+        all_textures.append(texture_img)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"The script took {elapsed_time:.2f} seconds to execute.")
 
-    out_img = cv2.cvtColor(np.uint8(texture_img),cv2.COLOR_BGR2RGB)
-    cv2.imwrite(f"temp_textures/texture.jpg", out_img)
-    
-    return texture_img
+    return all_textures
+
+def generate_template():
+    global all_textures,template,mask,composite
+
+    overlay = Image.open(os.path.join(args.overlay_dir,
+                                  f"{len(all_textures)}_ingredient.png")).convert("RGBA").resize((512,512))
+    template,template_values,mask = generate_template_and_mask(len(all_textures),overlay)
+
+    composite = composite_ingredients(all_textures[::-1],template,template_values)
+
+    return [template, mask,composite]
+
 
 def generate_burger(ingredient,strength,mask_blur_strength,steps,cfg):
 
@@ -203,68 +228,74 @@ def generate_burger(ingredient,strength,mask_blur_strength,steps,cfg):
 
 with gr.Blocks() as demo:
 
-    #texture gen row
-    with gr.Row():
+    with gr.Tab("Input Textures"):
+        #texture gen row
+        with gr.Row():
 
-            with gr.Column():
-                controlnet_prompt_input = gr.Textbox(label="prompt")
-                controlnet_input_img = gr.Image(label="input img")
-                controlnet_conditioning_scale_input = gr.Slider(0, 1, 
-                    value=args.controlnet_str, label="controlnet_conditioning_scale")
-                controlnet_steps_input = gr.Slider(0, 150, value=args.steps,
-                    label="number of diffusion steps")
-                controlnet_cfg_input = gr.Slider(0,30,value=args.cfg_scale,label="cfg scale")
+                with gr.Column():
+                    controlnet_prompt_input = gr.Textbox(label="prompt")
+                    controlnet_input_img = gr.Image(label="input img")
+                    controlnet_conditioning_scale_input = gr.Slider(0, 1, 
+                        value=args.controlnet_str, label="controlnet_conditioning_scale")
+                    controlnet_steps_input = gr.Slider(0, 150, value=args.steps,
+                        label="number of diffusion steps")
+                    controlnet_cfg_input = gr.Slider(0,30,value=args.cfg_scale,label="cfg scale")
 
-                controlnet_inputs = [
-                    controlnet_prompt_input,
-                    controlnet_input_img,
-                    controlnet_conditioning_scale_input,
-                    controlnet_steps_input,
-                    controlnet_cfg_input,
-                ]
+                    controlnet_inputs = [
+                        controlnet_prompt_input,
+                        controlnet_input_img,
+                        controlnet_conditioning_scale_input,
+                        controlnet_steps_input,
+                        controlnet_cfg_input,
+                    ]
 
-            with gr.Column():
+                with gr.Column():
 
-                controlnet_output = gr.Image()
+                    controlnet_output = gr.Gallery()
 
-    with gr.Row():
+        with gr.Row():
 
-            controlnet_submit = gr.Button("Submit")
+                controlnet_submit = gr.Button("Submit")
+    with gr.Tab("Templates"):
+         template_ouput = gr.Gallery()
+         template_submit = gr.Button("Submit")
 
-    #burger gen row
-    with gr.Row():
+    with gr.Tab("Burger Gen"):
+        #burger gen row
+        with gr.Row():
 
-            with gr.Column():
-                img2img_strength = gr.Slider(0, 1, 
-                    value=args.img2img_strength, 
-                    label="img2img strength")
-                mask_blur_strength = gr.Slider(0, 9, 
-                    value=args.mask_blur, 
-                    label="mask blur")
-                img2img_steps_input = gr.Slider(0, 150, value=args.steps,
-                    label="number of diffusion steps")
-                img2img_cfg_input = gr.Slider(0,30,value=args.cfg_scale,
-                                                 label="cfg scale")
+                with gr.Column():
+                    img2img_strength = gr.Slider(0, 1, 
+                        value=args.img2img_strength, 
+                        label="img2img strength")
+                    mask_blur_strength = gr.Slider(0, 9, 
+                        value=args.mask_blur, 
+                        label="mask blur")
+                    img2img_steps_input = gr.Slider(0, 150, value=args.steps,
+                        label="number of diffusion steps")
+                    img2img_cfg_input = gr.Slider(0,30,value=args.cfg_scale,
+                                                    label="cfg scale")
 
-                img2img_inputs = [
-                    #use the same prompt from step 1
-                    controlnet_prompt_input,
-                    img2img_strength,
-                    mask_blur_strength,
-                    img2img_steps_input,
-                    img2img_cfg_input,
-                ]
+                    img2img_inputs = [
+                        #use the same prompt from step 1
+                        controlnet_prompt_input,
+                        img2img_strength,
+                        mask_blur_strength,
+                        img2img_steps_input,
+                        img2img_cfg_input,
+                    ]
 
-            with gr.Column():
+                with gr.Column():
 
-                img2img_output = gr.Image()
+                    img2img_output = gr.Image()
 
-    with gr.Row():
+        with gr.Row():
 
-            img2img_submit = gr.Button("Submit")
+                img2img_submit = gr.Button("Submit")
 
 
     controlnet_submit.click(generate_texture,inputs=controlnet_inputs,outputs=controlnet_output)
+    template_submit.click(generate_template,outputs=template_ouput)
     img2img_submit.click(generate_burger,inputs=img2img_inputs,outputs=img2img_output)
 
 
