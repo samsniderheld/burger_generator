9d4475ba866b04891d6d7793aee409ab3b6ae824
samsniderheld <samsniderheld@gmail.com>
2023-11-06 13:08:20 -0500

    Update pipelines.py

--- pipelines/pipelines.py
+++ pipelines/pipelines.py
@@ -5,34 +5,70 @@ This module provides utility classes and functions to facilitate the process of 
 using various pipelines. The pipelines interface with pre-trained models from the `diffusers` 
 and `compel` modules to produce images based on provided textual prompts.
 
-Classes:
-    ControlNetPipeline: 
-        A pipeline wrapper around the `StableDiffusionControlNetPipeline` 
-        for image generation based on the ControlNet model.
-
-    Img2ImgPipeline:
-        A pipeline wrapper around the `StableDiffusionImg2ImgPipeline` for image-to-image 
-        conversion tasks.
-
-Usage Example:
-    controlnet_pipe = ControlNetPipeline(pipeline_path, controlnet_path)
-    generated_image = controlnet_pipe.generate_img(prompt, img, controlnet_str, width, height, steps, cfg)
-
-    img2img_pipe = Img2ImgPipeline(pipeline_path)
-    converted_image = img2img_pipe.generate_img(prompt, input_img, strength, steps, cfg)
-
 """
 import numpy as np
 import torch
 import cv2
 from PIL import Image
+import random
 # Import necessary modules from the diffusers package
 from diffusers import (ControlNetModel,
                        StableDiffusionXLControlNetPipeline,
                        AutoPipelineForInpainting,
                        DPMSolverMultistepScheduler
 )
- 
+
+from compel import Compel, ReturnedEmbeddingsType
+
+class InpaintingSDXLPipeline():
+    def __init__(self, pipeline_path):
+        # Store the path for the pipeline
+        self.pipeline_path = pipeline_path
+        # Load the pipeline upon initialization
+        self.load_pipeline()
+
+    def load_pipeline(self):
+        # Load the Image-to-Image pipeline
+        sdxl_pipe = AutoPipelineForInpainting.from_pretrained(
+                    self.pipeline_path,
+                    torch_dtype=torch.float16,
+                    variant="fp16").to("cuda")
+
+        sdxl_pipe.scheduler =  DPMSolverMultistepScheduler.from_config(sdxl_pipe.scheduler.config, use_karras=True, euler_at_final=True)   
+
+        self.pipeline = sdxl_pipe
+        
+        self.compel = Compel(
+          tokenizer=[sdxl_pipe.tokenizer, sdxl_pipe.tokenizer_2] ,
+          text_encoder=[sdxl_pipe.text_encoder, sdxl_pipe.text_encoder_2],
+          returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,
+          requires_pooled=[False, True]
+        )
+
+    def generate_img(self, prompt,negative_prompt,input_img, mask_img, strength, cfg, steps):        
+
+
+        conditioning, pooled = self.compel(prompt)
+
+        seed = random.randint(0,10000)
+
+        generator = torch.Generator(device='cuda').manual_seed(1234)
+
+        img = self.pipeline(
+            # prompt=prompt,
+            prompt_embeds=conditioning,
+            pooled_prompt_embeds=pooled,
+            negative_prompt=negative_prompt,
+            image= input_img,
+            mask_image=mask_img,
+            strength=strength,
+            guidance_scale=cfg,
+            generator=generator,
+            num_inference_steps=steps,
+        ).images[0]
+                
+        return img
+
 class ControlnetSDXLPipeline():
     def __init__(self, pipeline_path):
         # Store the path for the pipeline
@@ -54,7 +90,7 @@ class ControlnetSDXLPipeline():
             use_safetensors=True
         ).to('cuda')
 
-        sdxl_pipe.scheduler =  DPMSolverMultistepScheduler.from_config(sdxl_pipe.scheduler.config, use_karras=True)   
+        # sdxl_pipe.scheduler =  DPMSolverMultistepScheduler.from_config(sdxl_pipe.scheduler.config, use_karras=True)   
         
         self.pipeline = sdxl_pipe
 
@@ -79,34 +115,3 @@ class ControlnetSDXLPipeline():
                 
         return img
 
-class InpaintingSDXLPipeline():
-    def __init__(self, pipeline_path):
-        # Store the path for the pipeline
-        self.pipeline_path = pipeline_path
-        # Load the pipeline upon initialization
-        self.load_pipeline()
-
-    def load_pipeline(self):
-        # Load the Image-to-Image pipeline
-        sdxl_pipe = AutoPipelineForInpainting.from_pretrained(
-                    self.pipeline_path,
-                    torch_dtype=torch.float16,
-                    variant="fp16").to("cuda")
-
-        sdxl_pipe.scheduler =  DPMSolverMultistepScheduler.from_config(sdxl_pipe.scheduler.config, use_karras=True)   
-        
-        self.pipeline = sdxl_pipe
-
-    def generate_img(self, prompt,negative_prompt,input_img, mask_img, strength, cfg, steps):        
-
-        img = self.pipeline(
-            prompt=prompt,
-            negative_prompt=negative_prompt,
-            image= input_img,
-            mask_image=mask_img,
-            strength=strength,
-            guidance_scale=cfg,
-            num_inference_steps=steps,
-        ).images[0]
-                
-        return img
