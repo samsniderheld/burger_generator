46e44d1dd0f38aadfe5e709c5191c24f52ddb34c
samsniderheld <samsniderheld@gmail.com>
2023-11-08 22:45:34 -0500

    Update generate_sdxl.py

--- generate_sdxl.py
+++ generate_sdxl.py
@@ -145,7 +145,7 @@ row_1 = np.hstack(imgs[5:])
 grid = np.vstack([row_0,row_1])
 print(grid.shape)
 # grid = cv2.cvtColor(np.uint8(grid), cv2.COLOR_BGR2RGB)
-cv2.imwrite("{args.output_dir}/grid.jpg",grid)
+cv2.imwrite(f"{args.output_dir}/grid.jpg",grid)
 
 
 
